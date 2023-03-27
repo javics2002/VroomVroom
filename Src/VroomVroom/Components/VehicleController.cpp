3e8aed03bab4e3b326263505418c99fb4f00ac32
DiegoRol69 <82502179+DiegoRol69@users.noreply.github.com>
2023-03-27 19:33:15 +0200

    Cambio para mover el coche

--- Src/VroomVroom/Components/VehicleController.cpp
+++ Src/VroomVroom/Components/VehicleController.cpp
@@ -26,15 +26,15 @@ Returns the difference between this vector and another vector
 void VehicleController::update()
 {
     // Get the input
-    bool acelerate = me::inputManager().getButton("ACELERATE");
-    bool decelerate = me::inputManager().getButton("DECELERATE");
-    bool drift = me::inputManager().getButton("DRIFT");
-    bool useObjeto = me::inputManager().getButton("USEOBJECT");
+    //bool acelerate = me::inputManager().getButton("ACELERATE");
+    //bool decelerate = me::inputManager().getButton("DECELERATE");
+    //bool drift = me::inputManager().getButton("DRIFT");
+    //bool useObjeto = me::inputManager().getButton("USEOBJECT");
 
-    //If the player is using keyboard
-    bool left = me::inputManager().getButton("LEFT");
-    bool right = me::inputManager().getButton("RIGHT");
-    float deltaX = me::inputManager().getAxis("HORIZONTAL");
+    ////If the player is using keyboard
+    //bool left = me::inputManager().getButton("LEFT");
+    //bool right = me::inputManager().getButton("RIGHT");
+    //float deltaX = me::inputManager().getAxis("HORIZONTAL");
 
     /*
     // Get the input
@@ -51,32 +51,32 @@ void VehicleController::update()
 
 
     // Rotate the vehicle if the player is using DualShock or Xbox controller
-    if(deltaX != 0) {
-        if (drift) {
-            // If the vehicle is drifting, rotate it based on the drift factor
-            mEntity->getComponent<me::Transform>("transform")->rotate( mRotationSpeed * deltaX * mDriftFactor, me::AXIS_ROTATIONS_Y);
-        }
-        else {
-            // Otherwise, rotate it normally
-            mEntity->getComponent<me::Transform>("transform")->rotate(mRotationSpeed * deltaX, me::AXIS_ROTATIONS_Y);
-        }
-    }
-    //If the player is using keyboard
-    else if(left)
-        mEntity->getComponent<me::Transform>("transform")->rotate(-mRotationSpeed, me::AXIS_ROTATIONS_Y);
-    else if(right)
-        mEntity->getComponent<me::Transform>("transform")->rotate(mRotationSpeed, me::AXIS_ROTATIONS_Y);
+    //if(deltaX != 0) {
+    //    if (drift) {
+    //        // If the vehicle is drifting, rotate it based on the drift factor
+    //        mEntity->getComponent<me::Transform>("transform")->rotate( mRotationSpeed * deltaX * mDriftFactor, Vector3(0,1,0));
+    //    }
+    //    else {
+    //        // Otherwise, rotate it normally
+    //        mEntity->getComponent<me::Transform>("transform")->rotate(mRotationSpeed * deltaX, Vector3(0, 1, 0));
+    //    }
+    //}
+    ////If the player is using keyboard
+    //else if(left)
+    //    mEntity->getComponent<me::Transform>("transform")->rotate(-mRotationSpeed, Vector3(0, 1, 0));
+    //else if(right)
+    //    mEntity->getComponent<me::Transform>("transform")->rotate(mRotationSpeed, Vector3(0, 1, 0));
 
     // Move the vehicle forward or backward
     me::Vector3 v;
-    if (acelerate) {
+    if (true) {
         // If the vertical input axis is positive, add a forward impulse to the vehicle's rigidbody
-        mEntity->getComponent<me::RigidBody>("transform")->addImpulse(v.forward()*mSpeed,mEntity->getComponent<me::Transform>("transform")->getPosition());
+        mEntity->getComponent<me::RigidBody>("rigidbody")->addImpulse(v.left()*mSpeed,mEntity->getComponent<me::Transform>("transform")->getPosition());
     }
-    else if (decelerate) {
-        // If the vertical input axis is negative, add a backward impulse to the vehicle's rigidbody
-        mEntity->getComponent<me::RigidBody>("transform")->addImpulse(v.forward()*-mSpeed,mEntity->getComponent<me::Transform>("transform")->getPosition());
-    }
+    //else if (decelerate) {
+    //    // If the vertical input axis is negative, add a backward impulse to the vehicle's rigidbody
+    //    mEntity->getComponent<me::RigidBody>("transform")->addImpulse(v.forward()*-mSpeed,mEntity->getComponent<me::Transform>("transform")->getPosition());
+    //}
 }
 
 void VehicleController::setInput(std::string left, std::string right, std::string deltaX, std::string acelerate, std::string decelerate, std::string drift, std::string useObject)
