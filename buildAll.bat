526fa3a48ddaa7dad58c35055d9c2f86b3bb1a8d
josemiguelvdz <josemiguelv2002@gmail.com>
2023-05-04 14:26:27 +0200

    .Bats definitivos

--- buildAll.bat
+++ buildAll.bat
@@ -10,6 +10,8 @@ set VROOM_VROOM_SOL=.\VroomVroom.sln
 set VROOM_VROOM_DLL_DIR=.\Bin\x64\
 set ENGINE_EXE_DIR=.\MotorEngine\Exe\Main\x64\
 
+set VROOM_VROOM_ASSETS_DIR=.\Assets\
+
 rem Referencia a la herramienta de compresión: 
 set "zip=.\MotorEngine\Dependencies\7zip\7za.exe" 
 
@@ -36,10 +38,10 @@ robocopy /NJH %VROOM_VROOM_DLL_DIR%\Release\ %ENGINE_EXE_DIR%\Release\ "VroomVro
 
 rem CREAR BUILD DE RELEASE Y DEBUG EN 7ZIP
 set "source=.\MotorEngine\Exe\Main\x64\Debug\
-%zip% a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on build_final_x64_Debug.7z %source%/
+%zip% a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on Vroom_Vroom_x64_Debug.zip %source%/ %VROOM_VROOM_ASSETS_DIR%
 
 set "source=.\MotorEngine\Exe\Main\x64\Release\
-%zip% a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on build_final_x64_Release.7z %source%/
+%zip% a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=on Vroom_Vroom_x64_Release.zip %source%/ %VROOM_VROOM_ASSETS_DIR%
 
 pause
 exit
